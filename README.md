32c07dc9d3ccdf220f33597167f010b623aeee00
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2020-10-26 14:13:43 +0000

    Initial commit, adding githooks submodule

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# dataworks-repo-template-docker
+# ucfs-claimant-kafka-consumer
 
-## Description
+## UCFS Claimant Kafka Consumer
 
 This repo contains Makefile, and Dockerfile to fit the standard pattern.
 This repo is a base to create new Docker image repos, adding the githooks submodule, making the repo ready for use.
--- ecr.tf
+++ ecr.tf
@@ -1,16 +1,16 @@
-resource "aws_ecr_repository" "dataworks-repo-template-docker" {
-  name = "dataworks-repo-template-docker"
+resource "aws_ecr_repository" "ucfs-claimant-kafka-consumer" {
+  name = "ucfs-claimant-kafka-consumer"
   tags = merge(
     local.common_tags,
-    { DockerHub : "dwpdigital/dataworks-repo-template-docker" }
+    { DockerHub : "dwpdigital/ucfs-claimant-kafka-consumer" }
   )
 }
 
-resource "aws_ecr_repository_policy" "dataworks-repo-template-docker" {
-  repository = aws_ecr_repository.dataworks-repo-template-docker.name
+resource "aws_ecr_repository_policy" "ucfs-claimant-kafka-consumer" {
+  repository = aws_ecr_repository.ucfs-claimant-kafka-consumer.name
   policy     = data.terraform_remote_state.management.outputs.ecr_iam_policy_document
 }
 
 output "ecr_example_url" {
-  value = aws_ecr_repository.dataworks-repo-template-docker.repository_url
+  value = aws_ecr_repository.ucfs-claimant-kafka-consumer.repository_url
 }

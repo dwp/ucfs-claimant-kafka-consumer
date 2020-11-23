3ea0ea4b7c4474fffba78fd816a4af2a32b5c268
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2020-11-23 14:10:40 +0000

    Include region in ACM command

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,6 +39,7 @@ then
             --private-key-password "${KAFKA_PRIVATE_KEY_PASSWORD}" \
             --truststore-path "${KAFKA_TRUSTSTORE_PATH}" \
             --truststore-password "${KAFKA_TRUSTSTORE_PASSWORD}"
+            --region "${AWS_REGION}"
 
         echo "Cert generation result is $? for ${HOSTNAME}"
 

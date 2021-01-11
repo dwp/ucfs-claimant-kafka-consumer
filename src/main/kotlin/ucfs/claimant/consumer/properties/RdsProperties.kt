31923157ea81712728952590fcc3c7331f0f3c55
Daniel Chicot <dan.chicot@gmail.com>
2021-01-11 11:27:46 +0000

    Add alternate certificates and change default. (#32)
    * Add alternate certificates and change default.
    
    * Removed duplicate certificate.

--- src/main/kotlin/ucfs/claimant/consumer/properties/RdsProperties.kt
+++ src/main/kotlin/ucfs/claimant/consumer/properties/RdsProperties.kt
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 @ConfigurationProperties(prefix = "rds")
-data class RdsProperties(var caCertPath: String = "./rds-ca-2019-2015-root.pem",
+data class RdsProperties(var caCertPath: String = "./rds-ca-2019-eu-west-2.pem",
                          var claimantTable: String = "claimant",
                          var contractTable: String = "contract",
                          var statementTable: String = "statement",

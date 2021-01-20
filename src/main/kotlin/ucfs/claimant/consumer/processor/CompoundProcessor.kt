5960a47372f1af240c826fe61515db1945ce53f5
Daniel Chicot <dan.chicot@gmail.com>
2021-01-20 16:36:43 +0000

    IntelliJ analysis.

--- src/main/kotlin/ucfs/claimant/consumer/processor/CompoundProcessor.kt
+++ src/main/kotlin/ucfs/claimant/consumer/processor/CompoundProcessor.kt
@@ -2,7 +2,6 @@ package ucfs.claimant.consumer.processor
 
 import ucfs.claimant.consumer.domain.FilterProcessingOutput
 import ucfs.claimant.consumer.domain.JsonProcessingResult
-import ucfs.claimant.consumer.domain.TransformationProcessingOutput
 
 interface CompoundProcessor {
     fun process(input: JsonProcessingResult): FilterProcessingOutput

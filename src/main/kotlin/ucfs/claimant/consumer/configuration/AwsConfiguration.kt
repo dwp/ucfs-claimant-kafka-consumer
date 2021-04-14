d56ae646253b2e7a09f9de87c6ecdda71cb0abe9
Daniel Chicot <dan.chicot@gmail.com>
2021-04-14 19:14:24 +0100

    Configurable region for the kms key.

--- src/main/kotlin/ucfs/claimant/consumer/configuration/AwsConfiguration.kt
+++ src/main/kotlin/ucfs/claimant/consumer/configuration/AwsConfiguration.kt
@@ -3,18 +3,23 @@ package ucfs.claimant.consumer.configuration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.kms.KmsClient
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient
 import software.amazon.awssdk.services.ssm.SsmClient
 
 @Configuration
 @Profile("!LOCALSTACK")
-class AwsConfiguration {
+class AwsConfiguration(private val kmsRegion: String) {
     @Bean
     fun ssmClient(): SsmClient = SsmClient.create()
 
     @Bean
-    fun kmsClient(): KmsClient = KmsClient.create()
+    fun kmsClient(): KmsClient =
+        with (KmsClient.builder()) {
+            region(Region.of(kmsRegion))
+            build()
+        }
 
     @Bean
     fun secretsManagerClient(): SecretsManagerClient = SecretsManagerClient.create()
